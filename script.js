2bcf13257b3a7b1d2c902a451a7778097de466b1
tchkoidze <ilia.chkoidze12@gmail.com>
2023-03-06 15:43:34 +0400

    delete console.logs

--- script.js
+++ script.js
@@ -14,41 +14,26 @@ pageviewsRange.forEach((i) => {
   i.addEventListener("input", (event) => {
     x = event.target.value;
     /*slider range background size*/
-    event.target.style.backgroundSize =
-      (event.target.value * 100) / i.max + "%";
 
     if (event.target.value == 1000000) {
-      console.log(console.log(x));
       amount.textContent = "$36.00";
       folowersNumber.textContent = "1M";
     } else if (500000 <= event.target.value && event.target.value < 1000000) {
       amount.textContent = "$24.00";
       folowersNumber.textContent = "500K";
-      console.log(console.log(x));
-      return x;
     } else if (100000 <= event.target.value && event.target.value < 500000) {
-      console.log(console.log(x));
-
       amount.textContent = "$16.00";
       folowersNumber.textContent = "100K";
-      return x;
     } else if (50000 <= event.target.value && event.target.value < 100000) {
-      console.log(console.log(x));
       amount.textContent = "$12.00";
       folowersNumber.textContent = "50K";
     } else {
       amount.textContent = "$08.00";
       folowersNumber.textContent = "10K";
-      console.log(console.log(x));
     }
 
-    /*event.target.style.backgroundSize =
-      ((event.target.value - event.target.min) * 100) /
-      (event.target.max - event.target.min);
-    console.log(
-      (event.target.style.backgroundSize =
-        (event.target.value * 100) / i.max + "%")
-    );*/
+    event.target.style.backgroundSize =
+      (event.target.value * 100) / i.max + "%";
   });
 });
 
